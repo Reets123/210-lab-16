f687ff56ce37f857c104413ae8ce832f961cd2e8
Reets123 <ibrahimalatig@gmail.com>
2024-10-02 05:50:09 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,84 @@
+// Lab 14: Color Class
+// COMSC-210 - Ibrahim Alatig 
+
+// UML template
+//-------------
+//     Color
+// - red: int
+// - green: int
+// - blue: int
+//--------------
+// + Color()
+// + Color(int,int,int)
+// + setRed(int): void
+// + setGreen(int): void
+// + setBlue(int): void
+// + getRed(): int
+// + getGreen(): int
+// + getBlue(): int
+// + getBlue(): int
+// + print(): void
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+class Color {
+    private:
+    int red;
+    int green; 
+    int blue;
+
+public:
+    // constructor
+    Color() : red(0), green(0), blue(0) {}
+
+    Color(int r, int g, int b) : red(r), green(g), blue(b){}
+
+    void setRed(int r) {
+        red = r;
+    }
+
+    void setGreen(int g) {
+        green = g;
+    }
+
+    void setBlue(int b){
+        blue = b;
+    }
+
+    int getRed() const {
+        return red;
+    }
+
+    int getGreen() const {
+        return green;
+    }
+
+    int getBlue() const {
+        return blue;
+    }
+
+    void print() const {
+        cout << "Color (R: " << red << ", G: " << green << ", B: " << blue << ")" << endl;
+    }
+
+};
+
+int main() {
+    Color color1(255, 0, 0); // red
+    Color color2(0, 255, 0); // Green
+    Color color3(0, 0, 255); // Blue
+    Color color4(255, 255, 0); // yellow
+    Color color5(0, 255, 255); // cyan
+
+    cout << "Color values:" << endl;
+    color1.print();
+    color2.print();
+    color3.print();
+    color4.print();
+    color5.print();
+
+    return 0;
+}
